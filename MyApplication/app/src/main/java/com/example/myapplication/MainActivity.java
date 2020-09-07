62c1f8d0bd661a0e2c44f503d378d4c20abbdf52
Vladislav Taraikovich <54981764+smuzdev@users.noreply.github.com>
2020-09-07 21:11:09 +0300

    Hello, World!

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", " counter = " + count);
+        }
+        
+        displayText();
+    }
+
+    private void displayText() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
+    }
+
+}
