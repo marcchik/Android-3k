df189aea267925231e722c99ff24eb5a1b68e64d
smuzdev <smuzdev@yandex.ru>
2020-12-01 02:04:26 +0300

    The app crashes when switching to landscape mode

--- LAB_05/app/src/main/java/com/smuzdev/lab_05/activities/MainActivity.java
+++ LAB_05/app/src/main/java/com/smuzdev/lab_05/activities/MainActivity.java
@@ -6,14 +6,19 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -21,6 +26,7 @@ import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,6 +42,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.smuzdev.lab_05.R;
+import com.smuzdev.lab_05.fragments.DetailsFragment;
+import com.smuzdev.lab_05.fragments.ThingsListFragment;
 import com.smuzdev.lab_05.helper.MyAdapter;
 import com.smuzdev.lab_05.models.Thing;
 
@@ -45,192 +53,26 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    GridLayoutManager gridLayoutManager;
-    RecyclerView mRecyclerView;
-    List<Thing> thingList;
-    Thing mThing;
-    ProgressDialog progressDialog;
-    EditText txt_search;
+    private FrameLayout listContainer;
+    private FrameLayout detailsContainer;
 
-    Toolbar toolbar;
-    NavigationView navigationView;
-    DrawerLayout drawerLayout;
-
-    Context context = this;
+    ThingsListFragment thingsListFragment = new ThingsListFragment();
+    DetailsFragment detailsFragment = new DetailsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ThingsListFragment thingsListFragment = new ThingsListFragment();
+        DetailsFragment detailsFragment = new DetailsFragment();
 
-        mRecyclerView = findViewById(R.id.recyclerView);
-        gridLayoutManager = new GridLayoutManager(MainActivity.this, 1);
-        mRecyclerView.setLayoutManager(gridLayoutManager);
-
-        //Drawer MENU (ВЫЖВИЖНОЕ МЕНЮ)
-        // <---- ----->
-        toolbar = findViewById(R.id.main_toolbar);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_view);
-
-        setSupportActionBar(toolbar);
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.app_name,
-                R.string.app_name
-        );
-
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.show_list_view:
-                        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                        break;
-                    case R.id.show_table_view:
-                        gridLayoutManager = new GridLayoutManager(MainActivity.this, 2);
-                        mRecyclerView.setLayoutManager(gridLayoutManager);
-                        break;
-                    case R.id.upload_thing:
-                        startActivity(new Intent(context, UploadActivity.class));
-                        break;
-                }
-                return true;
-            }
-        });
-        // <---- ----->
-
-        txt_search = findViewById(R.id.txt_searchText);
-
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading items...");
-
-        thingList = new ArrayList<>();
-
-        final MyAdapter myAdapter = new MyAdapter(MainActivity.this, thingList);
-        mRecyclerView.setAdapter(myAdapter);
-
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Things");
-        progressDialog.show();
-        ValueEventListener eventListener = databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                thingList.clear();
-
-                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
-
-                    Thing thing = itemSnapshot.getValue(Thing.class);
-                    thing.setKey(itemSnapshot.getKey());
-                    thingList.add(thing);
-                }
-                myAdapter.notifyDataSetChanged();
-                progressDialog.dismiss();
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                progressDialog.dismiss();
-            }
-        });
-
-        txt_search.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                filter(s.toString());
-            }
-
-            private void filter(String text) {
-
-                ArrayList<Thing> filterList = new ArrayList<>();
-                for (Thing thing : thingList) {
-
-                    if (thing.getThingName().toLowerCase().contains(text.toLowerCase())) {
-                        filterList.add(thing);
-                    }
-
-                }
-
-                myAdapter.filteredList(filterList);
-
-            }
-        });
-
-        registerForContextMenu(mRecyclerView);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.flFragment, thingsListFragment)
+                .addToBackStack(null)
+                .commit();
 
     }
 
-    @Override
-    public boolean onContextItemSelected(@NonNull final MenuItem item) {
-        switch (item.getItemId()) {
-            case 121:
-                startActivity(new Intent(getApplicationContext(), UpdateActivity.class)
-                        .putExtra("thingNameKey", thingList.get(item.getGroupId()).getThingName())
-                        .putExtra("thingDescriptionKey", thingList.get(item.getGroupId()).getThingDescription())
-                        .putExtra("thingDiscoveryDateKey", thingList.get(item.getGroupId()).getThingDiscoveryDate())
-                        .putExtra("thingDiscoveryPlaceKey", thingList.get(item.getGroupId()).getThingDiscoveryPlace())
-                        .putExtra("thingPickupPointKey", thingList.get(item.getGroupId()).getThingPickupPoint())
-                        .putExtra("userNameKey", thingList.get(item.getGroupId()).getUserName())
-                        .putExtra("userPhoneKey", thingList.get(item.getGroupId()).getUserPhone())
-                        .putExtra("userEmailKey", thingList.get(item.getGroupId()).getUserEmail())
-                        .putExtra("oldImageUrl", thingList.get(item.getGroupId()).getThingImage())
-                        .putExtra("key", thingList.get(item.getGroupId()).getKey())
-                );
-                return true;
-            case 122:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder
-                        .setTitle("Delete")
-                        .setMessage("Do you want to delete item?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Things");
-                                FirebaseStorage storage = FirebaseStorage.getInstance();
-                                StorageReference storageReference = storage.getReferenceFromUrl(thingList.get(item.getGroupId()).getThingImage());
-
-                                storageReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        reference.child(thingList.get(item.getGroupId()).getKey()).removeValue();
-                                        Toast.makeText(MainActivity.this, "Thing deleted", Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                        finish();
-                                    }
-                                });
-                            }
-                        })
-                        .setNegativeButton("Cancel", null).create().show();
-                return true;
-            default:
-                super.onContextItemSelected(item);
-        }
-        return false;
-    }
-
-
-    public void btn_uploadActivity(View view) {
-
-        startActivity(new Intent(this, UploadActivity.class));
-
-    }
 
 }
